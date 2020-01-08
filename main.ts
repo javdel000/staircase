8cc18caef1e13a613d0adb5ab4163af470de2c6c
javdel000 <58610616+javdel000@users.noreply.github.com>
2020-01-08 13:24:33 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,12 +10,12 @@ let Juan = sprites.create(img`
 . . . . 5 5 5 5 d d 5 5 . . . . 
 . . . . . f f 5 5 5 5 . . . . . 
 . . . . . f f 5 5 5 . . . . . . 
-. . . . f f f f f f f . . . . . 
+. . d d f f f f f f f d d . . . 
 . . . . . f f f f f . . . . . . 
 . . . . . f f f f f . . . . . . 
 . . . . . f f f f f . . . . . . 
 . . . . . f f f f f . . . . . . 
-. . . . . . f . f . . . . . . . 
+. . . . . 1 1 . 1 1 . . . . . . 
 `, SpriteKind.Player)
 for (let index = 0; index < 9; index++) {
     pause(100)
@@ -38,14 +38,14 @@ let Jesus = sprites.create(img`
 . . . . . d e d e d . . . . . . 
 . . . . . d d d d d . . . . . . 
 . . . . . d d 3 d d . . . . . . 
-. . . . . f d d d . . . . . . . 
-. . . . . f . . . . . . . . . . 
-. . . . . f . . . . . . . . . . 
-. . . . . f . . . . . . . . . . 
-. . . . . f . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+. . . . . f d d d f . . . . . . 
+. . d f f f 5 f 5 f f f d . . . 
+. . . . . f 5 f 5 f . . . . . . 
+. . . . . f 5 f 5 f . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . . f . f . . . . . . . 
+. . . . . 1 1 . 1 1 . . . . . . 
 `, SpriteKind.Player)
 for (let index = 0; index < 9; index++) {
     pause(100)
